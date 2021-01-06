b0a1ed3eea29995f303bea807b22a39463655f6d
GrzegorzNecka <strzypior@gmail.com>
2021-01-06 21:46:04 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 import "./style.css";
+import { Howl } from "howler";
 
 const images = {
   treasure: "💰",
   leafs: "🍃"
 };
+const sound = new Howl({
+  src: [
+    "http://res.cloudinary.com/fadhilyudho/video/upload/v1515565927/879601-128_tqagvn.mp3"
+  ]
+});
 
 const entry = document.querySelector(".entry");
 const treasure = document.querySelector(".treasure");
 
-treasure.addEventListener("cat", () => alert("wygrałeś  💪 "), {
-  once: true
-});
+treasure.addEventListener(
+  "info",
+  () => {
+    alert("wygrałeś  💪 ");
+    sound.play();
+  },
+  {
+    once: true
+  }
+);
 
 entry.addEventListener(
   "mouseenter",
   ({ fromElement: elem }) => {
     if (elem.className.includes("treasure")) {
       elem.innerText = images.treasure;
-      treasure.dispatchEvent(new CustomEvent("cat", { once: true }));
+      treasure.dispatchEvent(new CustomEvent("info", { once: true }));
     } else if (elem.className.includes("tree")) {
       elem.innerText = images.leafs;
     }
